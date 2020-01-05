64221c9c9bb1c05d17ba719191cd759274d5b0de
HASEBA Junya <7pairs@gmail.com>
2020-01-05 17:45:44 +0900

    Change license

--- project.clj
+++ project.clj
@@ -1,7 +1,21 @@
-(defproject sections-to-chapter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+; Copyright 2020 HASEBA Junya
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+
+(defproject sections-to-chapter "1.0.0"
+  :description "A tool for Re:VIEW. Merge section files into one chapter file."
+  :url "https://github.com/7pairs/sections-to-chapter"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns sections-to-chapter.core})
